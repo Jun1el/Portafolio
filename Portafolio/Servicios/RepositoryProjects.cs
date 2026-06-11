386fcdd33ec42d3f6e54eee667b5cf5eda2cc1a1
Jun1el <latorrevasquezandressebastian@gmail.com>
2026-06-11 15:30:57 -0500

    Update nuevo proyecto web fc

--- Portafolio/Servicios/RepositoryProjects.cs
+++ Portafolio/Servicios/RepositoryProjects.cs
@@ -30,10 +30,10 @@ namespace Portafolio.Servicios
                     ImgURL="https://t2informatik.de/en/wp-content/uploads/sites/2/2024/01/burn-down-chart-smartpedia.jpg"
                 },
                 new Proyecto {
-                    Titulo = "Proyecto del clasificador de papas",
-                    Descripcion = "Proyecto enfocado a clasificar papas defectuosas mediante segmentacion y entrenamiento de modelos de clasificacion usando un dataset publico " ,
-                    Link ="https://github.com/Jun1el/Project-Grafica-Papa",
-                    ImgURL="https://storage.googleapis.com/kaggle-datasets-images/2110080/3505874/a436a8e7b5bd3fcc953462e9b2cce68e/dataset-cover.png?t=2023-08-21-13-35-34"
+                    Titulo = "WEB-FC - Portal de Quejas y Noticias",
+                    Descripcion = "Desarrollo web para la gestión de quejas y noticias de la Facultad de Ciencias de la Computación" ,
+                    Link ="https://github.com/Jun1el/WEB-FC",
+                    ImgURL="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Uni-logo_transparente_granate.png/250px-Uni-logo_transparente_granate.png"
                 },
             };
         }
